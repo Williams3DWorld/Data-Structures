155abcb69362f0316510c372ef27520dd4c8bf37
Williams3DWorld <williamtwheate@hotmail.com>
2019-12-29 17:55:53 +0000

    New Update

--- LinkedList/LinkedList/main.cpp
+++ LinkedList/LinkedList/main.cpp
@@ -3,19 +3,32 @@
 int main(int argc, char* argv[])
 {
 	/* Basic node struct */
-	struct node
+	struct node_s
 	{
 		int		data;
-		node*	next;
+		node_s*	next;
 	};
 
-	list::linkedlist_s<node> list;
+	/* Basic node struct */
+	struct node_d
+	{
+		int		data;
+		node_d* next;
+		node_d* prev;
+	};
 
-	list.emplace_back(1);
-	list.emplace_back(36);
-	list.emplace_back(42);
+	list::linkedlist_d<node_d> list;
 
-	list.display();
+	list.emplace_front(new node_d{ 4, nullptr, nullptr });
+	list.emplace_front(new node_d{ 6, nullptr, nullptr });
+	list.emplace_front(new node_d{ 8, nullptr, nullptr });
+
+	list.make_circular();
+
+	std::cout << "Tail next: " << list.get(2)->next->data << "\n";
+	std::cout << "Head prev: " << list.get(0)->prev->data << "\n";
+
+	list.display(true);
 	std::cout << "\nSize: " << list.size() << "\n";
 
 	system("PAUSE");
